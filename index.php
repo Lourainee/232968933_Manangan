5f4451eedb721f2f4dbf566de20c30da409a9920
M307user <M307user@lab.uc-bcf.local>
2025-10-01 11:03:18 +0800

    PHP Exercises

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercises</title>
+</head>
+<body>
+<h3>Lab 02</h3>
+<ul>
+    <li><a href="intro.php">Indroduce Yourself</a></li>
+    <li><a href="math.php">Simple Math</a></li>
+    <li><a href="calculating.php">Area and Perimeter of a Rectangle</a></li>
+    <li><a href="tempe.php">Temperature Converter</a></li>
+    <li><a href="swapvaria.php">Swapping Variables</a></li>
+    <li><a href="calcu.php">Salary Calculator</a></li>
+    <li><a href="bmiCalcu.php">BMI Calculator</a></li>
+    <li><a href="string.php">String Manipulation</a></li>
+    <li><a href="bank.php">Bank Account Simulation</a></li>
+    <li><a href="grades.php">Simple Grading System</a></li>
+    <li><a href="converter.php">Currency Converter</a></li>
+    <li><a href="estimator.php">Travel Cost Estimator</a></li>
+</ul>
+
+</body>
+</html>
